f02737ca6a859273eedc0a30fc340e3f686ff702
seongeun123 <97885785+seongeun123@users.noreply.github.com>
2024-03-19 01:30:57 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const searchEl=document.querySelector('.search');
+const searchInputEl=searchEl.querySelector('input');
+
+searchEl.addEventListener('Click', function () {
+    /*클릭했을 때 로직*/
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색'); 
+});
+
+searchInputEl.addEventListener('blur', function() { /*클래스를 지우는 함수*/
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', ''); 
+});
